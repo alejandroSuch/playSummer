9046eeacf15218da7df1cc8278ec78b4afd642a0
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-06 15:33:45 +0200

    Context initialization and CORS enabling

--- app/Global.java
+++ app/Global.java
@@ -3,8 +3,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import play.Application;
 import play.GlobalSettings;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.libs.F.Promise;
+import play.mvc.SimpleResult;
 
 public class Global extends GlobalSettings {
+    private class ActionWrapper extends Action.Simple {
+        public ActionWrapper(Action<?> action) {
+            this.delegate = action;
+        }
+
+        @Override
+        public Promise<SimpleResult> call(Http.Context ctx) throws java.lang.Throwable {
+            Promise<SimpleResult> call = this.delegate.call(ctx);
+            Http.Response response = ctx.response();
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            return call;
+        }
+    }
 
     private ApplicationContext ctx;
 
@@ -16,6 +34,11 @@ public class Global extends GlobalSettings {
     @Override
     public <A> A getControllerInstance(Class<A> clazz) throws BeansException {
         return ctx.getBean(clazz);
+    }
+
+    @Override
+    public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
+        return new ActionWrapper(super.onRequest(request, actionMethod));
     }
 
 }
