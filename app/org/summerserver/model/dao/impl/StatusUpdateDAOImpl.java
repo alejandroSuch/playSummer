962553d33fdcbe2d717a524fba7ab94a8bfdaf16
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-07 16:19:55 +0200

    deferred close management

--- app/org/summerserver/model/dao/impl/StatusUpdateDAOImpl.java
+++ app/org/summerserver/model/dao/impl/StatusUpdateDAOImpl.java
@@ -1,11 +1,12 @@
 package org.summerserver.model.dao.impl;
 
-import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.summerserver.model.dao.StatusUpdateDAO;
 import org.summerserver.model.dao.base.hibernate.spring.impl.GenericHibernateSpringDAOImpl;
+import org.summerserver.model.vo.Comment;
 import org.summerserver.model.vo.StatusUpdate;
 
 import java.util.List;
@@ -17,11 +18,19 @@ public class StatusUpdateDAOImpl extends GenericHibernateSpringDAOImpl<StatusUpd
 
     @Override
     public StatusUpdate findStatusWithComments(Long id) {
+        //StatusUpdate statusUpdate = (StatusUpdate)getSession().get(StatusUpdate.class, id);
 
-        StatusUpdate statusUpdate = (StatusUpdate)getSession().get(StatusUpdate.class, id);
-        //StatusUpdate statusUpdate = getById(id, false);
-        getHibernateTemplate().merge(statusUpdate);
-        System.out.println("comments are: " + statusUpdate.getComments());
+        StatusUpdate statusUpdate = null;
+        SessionFactoryUtils.initDeferredClose(getHibernateTemplate().getSessionFactory());
+
+        try {
+            statusUpdate = getById(id, false);
+            List<Comment> comments = statusUpdate.getComments();
+            statusUpdate.setJsonComments(comments.size() > 0 ? comments : null);
+        } finally {
+            SessionFactoryUtils.processDeferredClose(getHibernateTemplate().getSessionFactory());
+        }
+
         return statusUpdate;
     }
 
