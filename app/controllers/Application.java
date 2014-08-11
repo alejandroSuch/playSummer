54adb60d0065140ede5db3c037d34566d0272584
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-11 18:04:17 +0200

    Rotues to manage OPTIONS requests

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,17 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Result rootOptions() {
+        return noContent();
+    }
+
+    public static Result options(String url) {
+        return noContent();
     }
 
 }
